ce6f57e75d11c5c5ecaea2a67606076d01c6fcfb
Tarek Stelzle <tarek.stelzle@gmx.de>
2020-12-08 09:42:49 +0100

    06 Day Python; added better solution for problem 2

--- python/06_day/script.py
+++ python/06_day/script.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def read_data():
     list_data = []
     with open('data.csv') as data:
@@ -18,12 +19,14 @@ def read_data():
 
     return list_data
 
+
 def print_groups(data: list):
     for elem in data:
         print(elem)
 
+
 def problem_one(data: list):
-    sum = 0
+    answer_sum = 0
     list_sum = []
     for group in data:
         single_answers = []
@@ -32,29 +35,65 @@ def problem_one(data: list):
                 if answer not in single_answers:
                     single_answers.append(answer)
         list_sum.append(single_answers)
-        sum += len(single_answers)
+        answer_sum += len(single_answers)
 
     print(list_sum)
-    return sum
+    return answer_sum
+
 
 def problem_two(data: list):
-    sum = 0
+    answer_sum = 0
     list_sum = []
     for group in data:
         group_answers = []
-        for key,person_answer in group.items():
+        for key, person_answer in group.items():
             for answer in person_answer:
                 taking_answer = True
-                for key, person_answer in group.items():
-                    if answer not in person_answer:
+                for key_2, person_answer_2 in group.items():
+                    if answer not in person_answer_2:
                         taking_answer = False
                 if taking_answer and answer not in group_answers:
                     group_answers.append(answer)
         list_sum.append(group_answers)
-        sum += len(group_answers)
+        answer_sum += len(group_answers)
 
     print(list_sum)
-    return sum
+    return answer_sum
+
+
+def read_data_2():
+    data_list = []
+    with open('data.csv') as data:
+        csv_reader = csv.reader(data, delimiter=' ')
+        person_count = 0
+        answer_string = ""
+        for row in csv_reader:
+            if not row:
+                group_data = str(person_count) + "," + answer_string
+                data_list.append(group_data)
+                person_count = 0
+                answer_string = ""
+            else:
+                person_count += 1
+                answer_string += row[0]
+        group_data = str(person_count) + "," + answer_string
+        data_list.append(group_data)
+
+    return data_list
+
+
+def problem_two_2(data: list):
+    answer_sum = 0
+    for group in data:
+        splitted = group.split(',')
+        person_count = int(splitted[0])
+        answer_string = splitted[1]
+        all_answered = [answer for answer in answer_string if answer_string.count(answer) == person_count]
+        all_answered_set = set(all_answered)
+        answer_sum += len(all_answered_set)
+
+    return answer_sum
+
 
 def main():
     data = read_data()
@@ -63,6 +102,12 @@ def main():
 
     solution_2 = problem_two(data)
     print(solution_2)
+
+    data_2 = read_data_2()
+    print(data_2)
+    solution_2_2 = problem_two_2(data_2)
+    print(solution_2_2)
+
 
 if __name__ == "__main__":
     main()

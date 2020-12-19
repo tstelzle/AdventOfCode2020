24e4479f91a4d087db6422a3f67777b03b7f02f3
Tarek Stelzle <tarek.stelzle@gmx.de>
2020-12-19 17:16:02 +0100

    11 Day Python - Part 2

--- python/11_day/script.py
+++ python/11_day/script.py
@@ -25,8 +25,67 @@ def print_seating_area(area: list):
         print()
 
 
-def get_current_state(area: list, row: int, column: int, get_value: bool):
+def get_current_state(area: list, row: int, column: int, get_value: bool, problem_two: bool = False,
+                      direction: str = ''):
     seat_char = area[row][column]
+    if problem_two:
+        if direction:
+            not_end_of_list = True
+            while seat_char == '.' and not_end_of_list:
+                if direction == 'left':
+                    if column > 0:
+                        column -= 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'right':
+                    if column + 1 < len(area[row]):
+                        column += 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'bottom':
+                    if row + 1 < len(area):
+                        row += 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'bottom_left':
+                    if column > 0 and row + 1 < len(area):
+                        column -= 1
+                        row += 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'bottom_right':
+                    if row + 1 < len(area):
+                        row += 1
+                        if column + 1 < len(area[row]):
+                            column += 1
+                        else:
+                            not_end_of_list = False
+                            row -= 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'top':
+                    if row > 0:
+                        row -= 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'top_left':
+                    if column > 0 and row > 0:
+                        row -= 1
+                        column -= 1
+                    else:
+                        not_end_of_list = False
+                elif direction == 'top_right':
+                    if row > 0:
+                        row -= 1
+                        if column + 1 < len(area[row]):
+                            column += 1
+                        else:
+                            not_end_of_list = False
+                            row += 1
+                    else:
+                        not_end_of_list = False
+                seat_char = area[row][column]
+
     if get_value:
         if seat_char == 'L':
             return EMPTY
@@ -38,47 +97,52 @@ def get_current_state(area: list, row: int, column: int, get_value: bool):
         return seat_char
 
 
-def get_new_state(area: list, row: int, column: int):
-    current_seat = get_current_state(area, row, column, False)
+def get_new_state(area: list, row: int, column: int, problem_two: bool = False):
+    if problem_two:
+        seat_value_compare = 5
+    else:
+        seat_value_compare = 4
+
+    current_seat = get_current_state(area, row, column, False, problem_two, '')
     if current_seat == '.':
         return '.'
-    else:
-        top = EMPTY
-        bottom = EMPTY
-        left = EMPTY
-        right = EMPTY
-        top_left = EMPTY
-        top_right = EMPTY
-        bottom_left = EMPTY
-        bottom_right = EMPTY
 
-        if row > 0:
-            top = get_current_state(area, row - 1, column, True)
-            if column > 0:
-                top_left = get_current_state(area, row - 1, column - 1, True)
-            if column + 1 < len(area[row]):
-                top_right = get_current_state(area, row - 1, column + 1, True)
+    top = EMPTY
+    bottom = EMPTY
+    left = EMPTY
+    right = EMPTY
+    top_left = EMPTY
+    top_right = EMPTY
+    bottom_left = EMPTY
+    bottom_right = EMPTY
 
+    if row > 0:
+        top = get_current_state(area, row - 1, column, True, problem_two, 'top')
         if column > 0:
-            left = get_current_state(area, row, column - 1, True)
+            top_left = get_current_state(area, row - 1, column - 1, True, problem_two, 'top_left')
         if column + 1 < len(area[row]):
-            right = get_current_state(area, row, column + 1, True)
+            top_right = get_current_state(area, row - 1, column + 1, True, problem_two, 'top_right')
 
-        if row + 1 < len(area):
-            bottom = get_current_state(area, row + 1, column, True)
-            if column > 0:
-                bottom_left = get_current_state(area, row + 1, column - 1, True)
-            if column + 1 < len(area[row]):
-                bottom_right = get_current_state(area, row + 1, column + 1, True)
+    if column > 0:
+        left = get_current_state(area, row, column - 1, True, problem_two, 'left')
+    if column + 1 < len(area[row]):
+        right = get_current_state(area, row, column + 1, True, problem_two, 'right')
 
-        seat_value = top + left + bottom + right + top_left + top_right + bottom_left + bottom_right
+    if row + 1 < len(area):
+        bottom = get_current_state(area, row + 1, column, True, problem_two, 'bottom')
+        if column > 0:
+            bottom_left = get_current_state(area, row + 1, column - 1, True, problem_two, 'bottom_left')
+        if column + 1 < len(area[row]):
+            bottom_right = get_current_state(area, row + 1, column + 1, True, problem_two, 'bottom_right')
 
-        if current_seat == 'L' and seat_value == 0:
-            return '#'
-        elif current_seat == '#' and seat_value >= 4:
-            return 'L'
-        else:
-            return current_seat
+    seat_value = top + left + bottom + right + top_left + top_right + bottom_left + bottom_right
+
+    if current_seat == 'L' and seat_value == 0:
+        return '#'
+    elif current_seat == '#' and seat_value >= seat_value_compare:
+        return 'L'
+    else:
+        return current_seat
 
 
 def count_symbols(area: list):
@@ -93,8 +157,8 @@ def count_symbols(area: list):
     return symbols
 
 
-def problem_one(area: list):
-    current_seating_area = area
+def problem(area: list, problem_two: bool = False):
+    current_seating_area = copy.deepcopy(area)
     empty_list = []
 
     for row_counter in current_seating_area:
@@ -108,16 +172,21 @@ def problem_one(area: list):
 
         for row in range(0, len(current_seating_area)):
             for column in range(0, len(current_seating_area[row])):
-                new_seating_area[row][column] = get_new_state(current_seating_area, row, column)
+                new_seating_area[row][column] = get_new_state(current_seating_area, row, column, problem_two)
 
     return new_seating_area
 
 
 def main():
     area = read_data()
-    result_1 = problem_one(area)
-    print_seating_area(result_1)
+
+    result_1 = problem(area)
+    # print_seating_area(result_1)
     print(count_symbols(result_1))
+
+    result_2 = problem(area, True)
+    # print_seating_area(result_2)
+    print(count_symbols(result_2))
 
 
 if __name__ == "__main__":

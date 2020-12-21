402243ba55e9d6d244469103cb97b01d46f0c36f
Tarek <tarek.stelzle@gmx.de>
2020-12-21 20:18:41 +0100

    13 Day Python - Part 2 unfinished

--- python/13_day/script.py
+++ python/13_day/script.py
@@ -22,7 +22,7 @@ def problem_one(data: dict):
         if bus != 'x':
             bus = int(bus)
             initial = timestamp // bus
-            departure = (initial +1) * bus
+            departure = (initial + 1) * bus
             new_difference = departure - timestamp
             if difference == -1:
                 difference = new_difference
@@ -33,11 +33,53 @@ def problem_one(data: dict):
     return bus_id * difference
 
 
+def pos_checker(timestamp: int, busses: list, pos):
+    if (timestamp + pos) % int(busses[pos]) == 0:
+        return True
+    else:
+        return False
+
+
+def problem_two(data: dict):
+    busses = data['busses']
+    timestamp = 0
+    timestamp_found = False
+    adding = 1
+    switch_adding = True
+
+    bus_pos = [busses.index(x) for x in busses if x != 'x']
+    max_pos = 0
+    for x in range(0, len(bus_pos)):
+        if int(busses[bus_pos[x]]) > int(busses[bus_pos[max_pos]]):
+            max_pos = x
+
+    while not timestamp_found:
+        timestamp += adding
+        print(timestamp)
+        short_pos_checker = lambda lst: pos_checker(timestamp, busses, lst)
+        departures = list(map(short_pos_checker, bus_pos))
+        if all(departures):
+            timestamp_found = True
+        if switch_adding:
+            if departures[max_pos]:
+                adding = int(busses[bus_pos[max_pos]])
+                switch_adding = False
+            else:
+                if timestamp % int(busses[bus_pos[0]]) == 0:
+                    adding = int(busses[bus_pos[0]])
+
+    return timestamp
+
+
 def main():
     data = read_data()
     print(data)
+
     result_1 = problem_one(data)
     print(result_1)
+
+    result_2 = problem_two(data)
+    print(result_2)
 
 
 if __name__ == "__main__":
